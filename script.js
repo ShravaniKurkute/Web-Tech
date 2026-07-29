e60431daa4c7a107223ff2e4c9aacc69711a6292
Shravani Kurkute <kurkuteshravani88@gmail.com>
2026-07-29 12:15:11 +0530

    Few changes

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
-fetch("navbar.html")
-    .then(response => response.text())
-    .then(data => {
-        document.getElementById("navbar-container").innerHTML = data;
-    });
+fetch("navbar.html").then(response => response.text())
+.then(data => {
+    document.getElementById("navbar-container").innerHTML = data;
+});
 
-fetch("footer.html")
-    .then(response => response.text())
-    .then(data => {
-        document.getElementById("footer-container").innerHTML = data;
-    });
+fetch("footer.html").then(response => response.text())
+.then(data => {
+    document.getElementById("footer-container").innerHTML = data;
+});
