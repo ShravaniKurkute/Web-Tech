17b017398b50cb435269ebb6fe9441246ac55244
Shravani Kurkute <kurkuteshravani88@gmail.com>
2026-08-03 03:17:11 +0530

    API Fetched

--- script.js
+++ script.js
@@ -7,3 +7,34 @@ fetch("footer.html").then(response => response.text())
 .then(data => {
     document.getElementById("footer-container").innerHTML = data;
 });
+
+
+const url = "https://jsonplaceholder.typicode.com/users";
+
+const apiCard = document.querySelector("#api-card");
+
+console.log("Getting data from api");
+
+const getapidata = async () => {
+
+    let response = await fetch(url);
+
+    console.log(response);
+    console.log(response.status);
+
+    let data = await response.json();
+
+    console.log(data);
+
+    for(let i = 0; i < 5; i++){
+
+        apiCard.innerHTML += `
+            <div class="guest">
+                <p>Name: ${data[i].name}</p>
+                <p>Email: ${data[i].email}</p>
+            </div>
+        `;
+    }
+};
+
+getapidata();
